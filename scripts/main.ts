127ee97ff9114cba10a35b070ad9bc41df3abdaf
Dáire McDermott <dairemcdermott@outlook.com>
2026-06-19 13:58:40 +0100

    updates comment

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,7 +1,7 @@
 import { world } from '@minecraft/server';
 import * as eventHandlers from './event_handlers/event-handlers';
 
-// trigger when a player joins/loads in
+// trigger when a player joins/respawns
 world.afterEvents.playerSpawn.subscribe(eventHandlers.handlePlayerSpawn);
 
 // trigger when an entity dies
